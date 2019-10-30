c0d1cc4197ecc2b8c1a5defad429bd52cc8e1b8e
Claire <claire.cy.t@gmail.com>
2019-10-30 11:03:22 -0400

    Add routerLink to new page as a child page of tab1

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
             path: '',
             loadChildren: () =>
               import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-          }
+          },
+          { path: 'activity-detail', loadChildren: '../activity-detail/activity-detail.module#ActivityDetailPageModule' }
         ]
       },
       {
