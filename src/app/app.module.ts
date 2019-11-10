3ec45085525ca0eb240760bc808dbe91f181e78c
Claire <claire.cy.t@gmail.com>
2019-11-10 17:58:44 -0500

    add fab button and modal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
